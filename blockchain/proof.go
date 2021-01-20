e52d7b6a5d7399529cd462b8e5e29ffa7187590c
Filip <filipkrayem@gmail.com>
2021-01-20 23:15:39 +0100

    Create the blockchain, add Proof of Work

---
+++ blockchain/proof.go
@@ -0,0 +1,79 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+const difficulty = 18
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProof(block *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-difficulty))
+	fmt.Println(target)
+	return &ProofOfWork{block, target}
+}
+
+func (pow *ProofOfWork) joinData(nonce int) []byte {
+	data := bytes.Join([][]byte{
+		pow.Block.PrevHash,
+		pow.Block.Data,
+		toBytes(nonce),
+		toBytes(difficulty),
+	}, []byte{})
+
+	return data
+}
+
+func (pow *ProofOfWork) Mine() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
+
+	nonce := 0
+
+	for nonce < math.MaxInt64 {
+		data := pow.joinData(nonce)
+		hash = sha256.Sum256(data)
+		intHash.SetBytes(hash[:])
+
+		fmt.Printf("\r%x", hash)
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+
+	}
+	fmt.Println()
+
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	hash := sha256.Sum256(pow.joinData(pow.Block.Nonce))
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
+
+func toBytes(n int) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, n)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return buff.Bytes()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/filipk999/go-chain
+
+go 1.16
